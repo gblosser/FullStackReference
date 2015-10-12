70e84e8c7fadbc60da7d56ac2ffb5f04b5b43b5e
gblosser <gblosser@tsdweb.com>
2015-10-12 13:46:46 -0400

    Added CustomerServiceTest Updated AutomobileServiceTest

--- TSD.Reference.Core/test/Services/AutomobileServiceTest.cs
+++ TSD.Reference.Core/test/Services/AutomobileServiceTest.cs
@@ -1,4 +1,7 @@
-﻿using Moq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
 using TSD.Reference.Core.Data;
 using TSD.Reference.Core.Entities;
 using TSD.Reference.Core.Services;
@@ -15,6 +18,46 @@ namespace TSD.Reference.Core.test.Services
 			 var aMockRepo = new Mock<IAutomobileRepository>();
 			aMockRepo.Setup(aItem => aItem.AddAutomobile(It.IsAny<Automobile>())).Returns(1);
 
+			aMockRepo.Setup(aItem => aItem.GetAutomobiles(It.IsAny<List<int>>())).Returns(
+				new List<Automobile>
+				{
+					new Automobile
+					{
+						Class = "Full size",
+						Code = "FCAR",
+						Color = "Grey",
+						Id = 1,
+						LocationId = 1,
+						Manufacturer = "Chevrolet",
+						Model = "Impala",
+						Name = "Chevy Impala",
+						Style = "Sedan",
+						VehicleNumber = "RENT-1",
+						VIN = "XYZ123ABC789chevy"
+					}
+				});
+
+			aMockRepo.Setup(aItem => aItem.AddAutomobileAsync(It.IsAny<Automobile>())).Returns(Task.FromResult(100));
+
+			aMockRepo.Setup(aItem => aItem.GetAutomobilesAsync(It.IsAny<List<int>>())).Returns(	Task.FromResult(
+				new List<Automobile>
+				{
+					new Automobile
+					{
+						Class = "Full size",
+						Code = "FCAR",
+						Color = "Grey",
+						Id = 1,
+						LocationId = 1,
+						Manufacturer = "Chevrolet",
+						Model = "Impala",
+						Name = "Chevy Impala",
+						Style = "Sedan",
+						VehicleNumber = "RENT-1",
+						VIN = "XYZ123ABC789async"
+					}
+				}));
+
 			_svc = new AutomobileService(aMockRepo.Object);
 		}
 
@@ -25,6 +68,45 @@ namespace TSD.Reference.Core.test.Services
 			var aReturn = _svc.AddAutomobile(new Automobile());
 
 			Assert.Equal(1, aReturn);
+		}
+
+		[Fact]
+		public void GetAutomobileTest()
+		{
+			var aCar = _svc.GetAutomobiles(new List<int> {1});
+
+			Assert.Equal(1, aCar.Count);
+			Assert.Equal(aCar.First().VIN, "XYZ123ABC789chevy");
+		}
+
+		[Fact]
+		public async Task AddAutomobileTestAsync()
+		{
+			var aResponse = await _svc.AddAutomobileAsync(new Automobile
+			{
+				Class = "Full size",
+				Code = "FCAR",
+				Color = "Grey",
+				LocationId = 1,
+				Manufacturer = "Chevrolet",
+				Model = "Impala",
+				Name = "Chevy Impala",
+				Style = "Sedan",
+				VehicleNumber = "RENT-1",
+				VIN = "XYZ123ABC789chevy"
+			});
+
+			Assert.Equal(100, aResponse);
+		}
+
+
+		[Fact]
+		public async Task GetAutomobileTestAsync()
+		{
+			var aCar = await _svc.GetAutomobilesAsync(new List<int> { 1 });
+
+			Assert.Equal(1, aCar.Count);
+			Assert.Equal(aCar.First().VIN, "XYZ123ABC789async");
 		}
 	}
 }
---
+++ TSD.Reference.Core/test/Services/CustomerServiceTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Moq;
+using TSD.Reference.Core.Data;
+using TSD.Reference.Core.Entities;
+using TSD.Reference.Core.Services;
+using Xunit;
+
+namespace TSD.Reference.Core.test.Services
+{
+	public class CustomerServiceTest
+	{
+		private readonly CustomerService _svc;
+
+		public CustomerServiceTest()
+		{
+			var aCustomerRepository = new Mock<ICustomerRepository>();
+			aCustomerRepository.Setup(aItem => aItem.GetCustomerByName(It.IsAny<string>())).Returns(
+				(string theName) =>
+					new Customer
+					{
+						AllowsAdditions = false,
+						AllowsAdditionalDrivers = false,
+						HasMaxRentalDays = false,
+						Id = 1,
+						MaxRentalDays = 28,
+						Name = theName
+					});
+
+			aCustomerRepository.Setup(aItem => aItem.AddCustomer(It.IsAny<Customer>())).Returns(
+				(Customer theCustomer) => theCustomer.Id);
+
+			_svc = new CustomerService(aCustomerRepository.Object);
+		}
+
+		[Fact]
+		public void GetCustomerByNameTest()
+		{
+			var aCustomer = _svc.GetCustomerByName("ABC Rental");
+
+			Assert.Equal("ABC Rental", aCustomer.Name);
+		}
+
+		[Fact]
+		public void AddCustomerTest()
+		{
+			var aNewCustId = new Random(DateTime.Now.Millisecond).Next();
+			var aNewCustomerId = _svc.AddCustomer(
+				new Customer
+				{
+					AllowsAdditions = false,
+					AllowsAdditionalDrivers = false,
+					HasMaxRentalDays = false,
+					Id = aNewCustId,
+					MaxRentalDays = 28,
+					Name = "ABC Rentals"
+				});
+
+			Assert.Equal(aNewCustId, aNewCustomerId);
+		}
+
+		[Fact]
+		public void UpdateCustomerTest()
+		{
+			var aCustomer =
+				new Customer
+				{
+					AllowsAdditions = false,
+					AllowsAdditionalDrivers = false,
+					HasMaxRentalDays = true,
+					Id = 1,
+					MaxRentalDays = 28,
+					Name = "ABC Rentals"
+				};
+
+			_svc.UpdateCustomer(aCustomer);
+
+			Assert.Equal(true, aCustomer.HasMaxRentalDays);		// not really testing anything here
+		}
+
+		[Fact]
+		public void DeleteCustomerTest()
+		{
+			var aCustomer =
+				new Customer
+				{
+					AllowsAdditions = false,
+					AllowsAdditionalDrivers = false,
+					HasMaxRentalDays = true,
+					Id = 1,
+					MaxRentalDays = 28,
+					Name = "ABC Rentals"
+				};
+
+			_svc.DeleteCustomer(aCustomer);		// nothing to assert on here
+
+		}
+	}
+}
